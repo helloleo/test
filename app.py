07369f7194e44a0990b7ae1c45af80789b1af8c8
Leo <leozhang.name@live.com>
2012-02-26 21:43:41 +0800

    添加文章函数

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 #coding=utf-8
 
-from flask import Flask, render_template
+import re
+
+from flask import Flask, request, render_template
 from flaskext.sqlalchemy import SQLAlchemy
 from datetime import datetime
+from markdown import markdown
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:helloleo@localhost/flask?charset=utf8'
@@ -39,20 +42,55 @@ class Category(db.Model):
     def __repr__(self):
         return '<Category %r>' % self.name
 
+def read(content):
+    meta_regex = re.compile(
+        r"^\s*(?:-|=){3,}\s*\n((?:.|\n)+?)\n\s*(?:-|=){3,}\s*\n*",
+        re.MULTILINE
+    )
+    match = re.match(meta_regex, content)
+    if not match:
+        logger.error("No metadata in: %s" % self.filepath)
+        return None
+    meta = match.group(1)
+    meta = re.sub(r'\r\n|\r|\n', '\n', meta)
+    dct = {}
+    k = v = None
+    for meta in meta.split('\n'):
+        meta = meta.replace('\t', '    ')
+        if meta.startswith('  ') and k:
+            dct[k] = dct[k] + '\n' + meta.lstrip()
+        if ':' in meta and not meta.startswith(' '):
+            index = meta.find(':')
+            k, v = meta[:index], meta[index + 1:]
+            k, v = k.rstrip(), v.lstrip()
+            dct[k] = v
+    text = content[match.end():]
+    dct['content'] = markdown(text)
+    dct['origin'] = content
+    return dct
+
 @app.route('/add/', methods=['POST', 'GET'])
 def add():
-    categorys = db.session.query(Category).all()
-    for category in categorys:
-        if new_category == category.name:
-            new_post = db.Post(new_title, new_content, category.id, new_date)
+    if request.method == 'GET':
+        return render_template('add.html')
+    if request.method == 'POST':
+        data = read(request.form['content'])
+        categorys = db.session.query(Category).all()
+        new_category = None
+        for category in categorys:
+            if data['category'] == category.name:
+                new_category = category
+        if new_category != None:
+            new_post = Post(data['title'], data['content'], new_category)
             db.session.add(new_post)
             db.session.commit()
         else:
-            new_category = db.Category(new_category)
-            new_post = db.Post(new_title, new_content, new_category, new_date)
+            new_category = Category(data['category'])
+            new_post = Post(data['title'], data['content'], new_category)
             db.session.add(new_category)
             db.session.add(new_post)
             db.session.commit()
+        return 'all ok'
 
 @app.route("/")
 def hello():
